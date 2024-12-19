f4790d9df80190267e65e18cf460587c7c71f6c0
Shaiful <shaiful48027@gmail.com>
2024-12-20 03:37:32 +0600

    update

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        "primary": "#0092E4",
+        "secondary": "#183656",
+        "paramain": "#1A1A1A",
+        "para": "#343D48"
+      }
+    },
+  },
+  plugins: [],
+}
+
